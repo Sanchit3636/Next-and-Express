601b47e27a129b260ea70c001d74fe501b6f2658
Matthew Stockinger <matt@matthewstockinger.com>
2023-04-19 13:04:32 -0500

    add delete endpoint

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -51,9 +51,18 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
 // delete a review
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-  // filter reviews based on session username.
-  // then delete those reviews.
-  res.status(200).json({ message: "Book review deleted." });
+  const user = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  if (!books[isbn]) {
+    res.status(400).json({ message: "invalid ISBN." });
+  } else if (!books[isbn].reviews[user]) {
+    res
+      .status(400)
+      .json({ message: `${user} hasn't submitted a review for this book.` });
+  } else {
+    delete books[isbn].reviews[user];
+    res.status(200).json({ message: "Book review deleted." });
+  }
 });
 
 module.exports.authenticated = regd_users;
